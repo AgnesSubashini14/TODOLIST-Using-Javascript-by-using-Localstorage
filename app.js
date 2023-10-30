cf4f83981c055c042cfc0af7222de7406c4be839
AgnesSubashini14 <144339818+AgnesSubashini14@users.noreply.github.com>
2023-10-30 11:19:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let button=document.getElementById('add');
+let todolist=document.getElementById('todolist');
+let input=document.getElementById('input');
+let todos=[];
+window.onload=()=>{
+    // console.log('loaded');
+  todos=JSON.parse (localStorage.getItem('todos')) || [] 
+  console.log(todos); 
+  todos.forEach(todo=>addtodo(todo))
+    
+  }
+    
+  
+
+button.addEventListener('click',()=>{
+    todos.push(input.value)
+    localStorage.setItem('todos',JSON.stringify(todos))
+    addtodo(input.value);
+    console.log(todos);
+    input.value='';
+})
+function addtodo(todo){
+    let para=document.createElement('p');
+    para.innerText=todo;
+    todolist.appendChild(para)
+    para.addEventListener('click',()=>{
+        para.style.textDecoration='line-through';
+        remove(todo)
+    })
+    para.addEventListener('dblclick',()=>{
+        todolist.removeChild(para);
+        remove(todo)
+    })
+}
+function remove(todo){
+    let index=todos.indexOf(todo)
+    if(index > -1){
+        todos.splice(index,1) 
+    }
+    localStorage.setItem('todos',JSON.stringify(todos))
+    
+}
